edaefd1839dbb74ebad0a5918c56fb35b2055a20
Jason <jasonluxe8719@gmail.com>
2020-07-06 16:53:17 -0500

    Second commit

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <footer>
+          Copyright &copy; 2020
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
