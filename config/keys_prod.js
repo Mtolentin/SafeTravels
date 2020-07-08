7764cd451b6a83d11deb571a4b7ac0f6e0e929dd
Mtolentin <mattca701@gmail.com>
2020-07-07 20:31:20 -0700

    Heroku 1 Take 12

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
-    secretOrKey: process.env.SECRET_OR_KEY
-    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME
+    secretOrKey: process.env.SECRET_OR_KEY,
+    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     AWS_REGION: process.env.AWS_REGION,
