49788f6b53daa8331bc94276a50bd7de252bfc8e
Mtolentin <mattca701@gmail.com>
2020-07-07 18:50:03 -0700

    Heroku 1.1

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
 // const path = require('path');
 
 const bodyParser = require('body-parser');
@@ -14,27 +13,55 @@ const posts = require("./routes/api/posts");
 const uploads = require("./routes/api/uploads");
 
 
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+//LOCAL VERSION
 
-app.get("/", (req, res) => res.send("Hello There!"));
+// const db = require('./config/keys').mongoURI;
+// mongoose
+//   .connect(db, { useNewUrlParser: true })
+//   .then(() => console.log("Connected to MongoDB successfully"))
+//   .catch(err => console.log(err));
 
-app.use(passport.initialize());
-require('./config/passport')(passport);
+// app.get("/", (req, res) => res.send("Hello There!"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(passport.initialize());
+// require('./config/passport')(passport);
 
-app.use("/api/users", users);
-app.use("/api/posts", posts);
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-app.use("/api/document", uploads);
+// app.use("/api/users", users);
+// app.use("/api/posts", posts);
 
-// app.use(express.static(path.join(__dirname, "build")));
+// app.use("/api/document", uploads);
 
-const port = process.env.PORT || 4000;
+// // app.use(express.static(path.join(__dirname, "build")));
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// const port = process.env.PORT || 4000;
 
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// HEROKU VERSION
+
+var http = require('http');         // For serving a basic web page.
+var mongoose = require("mongoose"); // The reason for this demo.
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring =
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/HelloMongoose';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+    console.log('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log('Succeeded connected to: ' + uristring);
+  }
+});
