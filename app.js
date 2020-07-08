831b57d3a56375df0e98e37f1e64f70d87c71e8f
Jason <jasonluxe8719@gmail.com>
2020-07-08 13:07:57 -0500

    Heroku Take 100

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/public'));
+  app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
 
