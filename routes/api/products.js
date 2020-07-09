063427cb519f908cd7267ecd4bbf5bf2e3481b72
Jason <jasonluxe8719@gmail.com>
2020-07-08 19:11:28 -0500

    Product Schema

--- routes/api/products.js
+++ routes/api/products.js
@@ -4,38 +4,13 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Product = require('../../models/Product');
-const { validateProductString, validateProductURL
-    } = require('../../validation/products');
+const validateProductInput = require('../../validation/products');
 
 router.post('/',
     passport.authenticate('jwt', { session: false }),
     
     (req, res) => {
-        //{errors, isValid}
-        let check = validateProductString(req.body.name);
-        check = validateProductString(req.body.category);
-        check = validateProductString(req.body.description);
-
-        if (req.body.rating < 1 || req.body.rating > 5) {
-            check.errors.text += 'Rating must by between 1 and 5';
-            check.isValid += Object.keys(errors).length === 0;
-        }
-
-        if (req.body.link1) {
-            let check = validateProductURL(req.body.link1);
-        }
-
-        if (req.body.link2) {
-            let check = validateProductURL(req.body.link2);
-        }
-
-        if (req.body.keyword1) {
-            let check = validateProductString(req.body.keyword1);
-        }
-
-        if (req.body.keyword2) {
-            let check = validateProductString(req.body.keyword2);
-        }
+        const {errors, isValid} = validateProductInput(req.body);
 
 
         if (!isValid) {
@@ -43,9 +18,9 @@ router.post('/',
         }
 
         const newProduct = new Product({
-            name: req.body.text,
+            name: req.body.name,
             category: req.body.category,
-            description: red.body.description,
+            description: req.body.description,
             rating: req.body.rating,
             link1: req.body.link1 || "",
             link2: req.body.link2 || "",
@@ -61,7 +36,6 @@ router.get('/', (req, res) => {
     Product.find()
         .sort({ date: -1 })
         .then(products => res.json(products))
-        .catch(err => res.status(404).json({ noproducts: 'No products found' }));
 });
 
 module.exports = router;
