1ada16ceb7a09eab5d9e619ed02973efbede058b
Mtolentin <mattca701@gmail.com>
2020-07-09 14:24:04 -0700

    14:24

---
+++ frontend/src/components/leftmodule/leftmodule_index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../../css/products.css';
+class LeftModuleIndex extends React.Component{
+    constructor(props) { super(props); }
+
+    componentDidMount() {
+        this.props.fetchProducts();
+    }
+
+    render() {
+
+        const {products} = this.props;
+
+        if (products[0] === undefined) {
+            return null;
+        }
+
+        let products_arr = products[0].map((product) => {
+
+                return (
+                    <li className="product" key={product._id}>
+                        <img className = "IProd1"
+                            src = {`${product.pictureURL}`}>
+                            </img>
+                        {product.name} | {product.description}
+                    </li>
+                )
+
+        })
+        return (
+            <div>
+                <ul className="posts-list">
+                    {products_arr}
+                </ul>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+export default LeftModuleIndex;
--- frontend/src/reducers/products_reducer.js
+++ frontend/src/reducers/products_reducer.js
@@ -1,12 +1,12 @@
 import {
-    RECIEVE_PRODUCTS
+    RECEIVE_PRODUCTS
 } from '../actions/product_actions';
 
 export default function (state = {}, action) {
     Object.freeze(state);
     let nextState = Object.assign({}, state);
     switch (action.type) {
-        case RECIEVE_PRODUCTS:
+        case RECEIVE_PRODUCTS:
             return action.products;
         default:
             return state;
