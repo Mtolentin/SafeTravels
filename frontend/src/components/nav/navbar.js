5aad190e48412c9b747e3607d7707c72488326f7
Khai Yuan Liew <kyuliew@gmail.com>
2020-07-21 17:19:06 -0700

    Restyled Index and Splash Pages

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../css/navbar.css';
 
+
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
