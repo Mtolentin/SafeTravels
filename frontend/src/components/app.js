692c163937601e700fd8bc74ee35892e4fe10110
Khai Yuan Liew <kyuliew@gmail.com>
2020-07-23 17:17:28 -0700

    Updated styling for index and splash page

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
-
-// import PostsContainer from './posts/posts_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
@@ -11,9 +9,7 @@ import SignupFormContainer from './session/signup_form_container';
 import PostsIndexContainer from './posts/posts_index_container';
 import MapContainer from './map/map_container';
 import Footer from './footer/footer';
-
-// import ProfileContainer from './profile/profile_container';
-// import PostComposeContainer from './posts/post_compose_container';
+// import SideNav from './sidenav/sidenav';
 
 
 const App = () => (
@@ -24,9 +20,9 @@ const App = () => (
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
     </Switch>
-      <div className="index-main">
-        <ProtectedRoute path="/index" component={MapContainer}/>
-        <ProtectedRoute path="/index" component={PostsIndexContainer} /> 
+        <div className="index-main">
+          <ProtectedRoute path="/index" component={MapContainer}/>
+          <ProtectedRoute path="/index" component={PostsIndexContainer} /> 
       </div>
       <Footer /> 
   </div> 
--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -12,80 +12,79 @@ class MainPage extends React.Component {
           <Carousel>
             <Carousel.Item>
               <img
-                className="d-block w-100"
-                src="http://www.cityofmaysville.com/_uploads/work-in-progress.png"
+                className="d-block w-100 carousel-img"
+                src="https://images.wallpaperscraft.com/image/street_traffic_urban_123634_800x600.jpg"
                 alt="First slide"
               />
               <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                <h3>Restaruants</h3>
+                <p>See which restaruants are still open during the pandemic.</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
-                className="d-block w-100"
-                src="http://www.cityofmaysville.com/_uploads/work-in-progress.png"
+                className="d-block w-100 carousel-img"
+                src="https://www.sfexaminer.com/wp-content/uploads/2019/02/f.WalkSF.0715.jpg"
                 alt="Second slide"
               />
 
               <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h3>Activites</h3>
+                <p>Find what activites you can do during the pandemic.</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
-                className="d-block w-100"
-                src="http://www.cityofmaysville.com/_uploads/work-in-progress.png"
+                className="d-block w-100 carousel-img"
+                src="https://media.atre.yardi.com/2/68521/images/San-Francisco-Proper-Hotel.jpg"
                 alt="Third slide"
               />
 
               <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                <h3>Hotels</h3>
+                <p>Find hotels that are open during the pandemic.</p>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
 
-          {/* <img id="image-content" src="http://www.cityofmaysville.com/_uploads/work-in-progress.png" /> */}
-          <p id="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Quisque pharetra sem at lectus maximus rhoncus.
-          Fusce ut ex vitae elit blandit cursus.
-          Phasellus urna dui, sodales venenatis justo at, faucibus aliquet massa.
-          Nunc et massa a arcu lobortis aliquam a et dolor.
-          Nunc ullamcorper ante nec finibus elementum.
-          Donec lacinia rhoncus ipsum, id tincidunt nulla semper in.
-          Ut mollis a lacus eget egestas.
-          Mauris tristique dapibus turpis volutpat dictum.
-          Mauris tempus urna lorem, id aliquet velit elementum sed.
-          Proin sapien risus, tincidunt vestibulum cursus vitae, bibendum non erat.
-          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          Maecenas vitae risus ac nisi gravida vehicula.
-          Aliquam accumsan rhoncus dui ac tempus.
-          Integer vitae vulputate nunc.
+          {/* <p id="description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Quisque pharetra sem at lectus maximus rhoncus.
+            Fusce ut ex vitae elit blandit cursus.
+            Phasellus urna dui, sodales venenatis justo at, faucibus aliquet massa.
+            Nunc et massa a arcu lobortis aliquam a et dolor.
+            Nunc ullamcorper ante nec finibus elementum.
+            Donec lacinia rhoncus ipsum, id tincidunt nulla semper in.
+            Ut mollis a lacus eget egestas.
+            Mauris tristique dapibus turpis volutpat dictum.
+            Mauris tempus urna lorem, id aliquet velit elementum sed.
+            Proin sapien risus, tincidunt vestibulum cursus vitae, bibendum non erat.
+            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Maecenas vitae risus ac nisi gravida vehicula.
+            Aliquam accumsan rhoncus dui ac tempus.
+            Integer vitae vulputate nunc.
 
 
-          Cras luctus magna odio, a sagittis lectus tincidunt et.
-          Sed semper, dui sit amet condimentum ornare, ante ipsum pulvinar tellus, in blandit diam velit ac dolor.
-          Fusce ac congue magna.
-          In ac massa sit amet nibh consequat dapibus commodo nec nisi.
-          Quisque sed nibh feugiat, maximus ante ut, facilisis sem.
-          Aenean vitae varius lacus.
-          Sed sagittis sem quis ligula commodo porttitor.
-          Donec finibus ipsum a urna vulputate congue.
+            Cras luctus magna odio, a sagittis lectus tincidunt et.
+            Sed semper, dui sit amet condimentum ornare, ante ipsum pulvinar tellus, in blandit diam velit ac dolor.
+            Fusce ac congue magna.
+            In ac massa sit amet nibh consequat dapibus commodo nec nisi.
+            Quisque sed nibh feugiat, maximus ante ut, facilisis sem.
+            Aenean vitae varius lacus.
+            Sed sagittis sem quis ligula commodo porttitor.
+            Donec finibus ipsum a urna vulputate congue.
 
 
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Cras lacinia nisl quis tempus cursus.
-          Duis non elit enim.
-          Ut molestie fringilla finibus.
-          Cras finibus felis erat, eu sollicitudin mi tempus interdum.
-          Nullam vitae felis eget sem ultrices mattis a eu metus.
-          Duis consequat nec quam ac fermentum.
-          Pellentesque ac nisi nisi.
-          In hac habitasse platea dictumst.
-          </p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras lacinia nisl quis tempus cursus.
+            Duis non elit enim.
+            Ut molestie fringilla finibus.
+            Cras finibus felis erat, eu sollicitudin mi tempus interdum.
+            Nullam vitae felis eget sem ultrices mattis a eu metus.
+            Duis consequat nec quam ac fermentum.
+            Pellentesque ac nisi nisi.
+            In hac habitasse platea dictumst.
+          </p> */}
         </div>
       </div>
     );
