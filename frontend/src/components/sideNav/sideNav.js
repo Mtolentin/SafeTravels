8e24e37844476b7bebe3323089baa04178ee3a25
Mtolentin <mattca701@gmail.com>
2020-07-23 17:07:21 -0700

    sideNav 3 07231707

--- frontend/src/components/sideNav/sideNav.js
+++ frontend/src/components/sideNav/sideNav.js
@@ -10,78 +10,80 @@ class SideNav extends React.Component{
 
     render() {
         return (
-            <div class="wrapper">
+            <div className="wrapper">
             <nav id="sidebar">
-                <div class="sidebar-header">
-                <button type="button" id="sidebarCollapse" class="btn btn-info">
-                            <i class="fas fa-align-left"></i>
+                <div className="sidebar-header">
+                <button type="button" id="sidebarCollapse" className="loggedin-btn">
+                            <i className="fas fa-align-left"></i>
                             <span>Toggle Sidebar</span>
                         </button>
-                        <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <i class="fas fa-align-justify"></i>
+                        <button className="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <i className="fas fa-align-justify"></i>
                         </button>
                 </div>
     
-                <ul class="list-unstyled components">
-                    <li class="active">
-                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                            <i class="fas fa-home"></i>
+                <ul className="list-unstyled components">
+                    <li className="active">
+                        <b href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">
+                            <i className="fas fa-home"></i>
                             Home
-                        </a>
-                        <ul class="collapse list-unstyled" id="homeSubmenu">
+                        </b>
+                        <ul className="collapse list-unstyled" id="homeSubmenu">
                             <li>
-                                <a href="#">Home 1</a>
+                                <p >Home 1</p>
                             </li>
                             <li>
-                                <a href="#">Home 2</a>
+                                <p >Home 2</p>
                             </li>
                             <li>
-                                <a href="#">Home 3</a>
+                                <p >Home 3</p>
                             </li>
                         </ul>
                     </li>
                     <li>
-                        <a href="#">
-                            <i class="fas fa-briefcase"></i>
+                        <b>
+                            <i className="fas fa-briefcase"></i>
                             About
-                        </a>
-                        <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                            <i class="fas fa-copy"></i>
+                        </b>
+                        <br></br>
+                        <b href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">
+                            <i className="fas fa-copy"></i>
                             Pages
-                        </a>
-                        <ul class="collapse list-unstyled" id="pageSubmenu">
+                        </b>
+                        <ul className="collapse list-unstyled" id="pageSubmenu">
                             <li>
-                                <a href="#">Page 1</a>
+                                <p>Page 1</p>
                             </li>
                             <li>
-                                <a href="#">Page 2</a>
+                                <p>Page 2</p>
                             </li>
                             <li>
-                                <a href="#">Page 3</a>
+                                <p>Page 3</p>
                             </li>
                         </ul>
                     </li>
                     <li>
-                        <a href="#">
-                            <i class="fas fa-image"></i>
+                        <b>
+                            <i className="fas fa-image"></i>
                             Portfolio
-                        </a>
+                        </b>
                     </li>
                     <li>
-                        <a href="#">
-                            <i class="fas fa-question"></i>
+                        <b>
+                            <i className="fas fa-question"></i>
                             FAQ
-                        </a>
+                        </b>
                     </li>
                     <li>
-                        <a href="#">
-                            <i class="fas fa-paper-plane"></i>
+                        <b>
+                            <i className="fas fa-paper-plane"></i>
                             Contact
-                        </a>
+                        </b>
                     </li>
                 </ul>
     
             </nav>
+
         </div>
         )
 
@@ -94,18 +96,18 @@ class SideNav extends React.Component{
         // let products_arr = products[0].map((product) => {
 
         //         return (
-        //             <li className="product" key={product._id}>
-        //                 <img className = "product-image"
+        //             <li classNameName="product" key={product._id}>
+        //                 <img classNameName = "product-image"
         //                     src = {`${product.pictureURL}`}>
         //                     </img>
-        //                 <div className="product-text"><p className="product-text-2">{product.name} | {product.description}</p></div>
+        //                 <div classNameName="product-text"><p classNameName="product-text-2">{product.name} | {product.description}</p></div>
         //             </li>
         //         )
 
         // })
         // return (
-        //     <div className="products">
-        //         <ul className="products-list">
+        //     <div classNameName="products">
+        //         <ul classNameName="products-list">
         //             {products_arr}
         //         </ul>
         //     </div>
