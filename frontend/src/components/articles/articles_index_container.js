8230232fa5de40dabaa6e3e372821561edc22970
Mtolentin <mattca701@gmail.com>
2020-07-27 23:39:53 -0700

    Articles work

---
+++ frontend/src/components/articles/articles_index_container.js
@@ -0,0 +1,31 @@
+import { connect } from 'react-redux';
+import { fetchPosts, deletePost, composePost, updatePost } from '../../actions/post_actions';
+import PostIndex from './posts_index';
+
+const mapStateToProps = (state) => {
+    const newPost = {
+        text: '',
+        author: { user: state.session.user.id }
+    }
+
+
+    return {
+        newPost: newPost,
+        posts: Object.values(state.entities.posts),
+        errors: state.errors.post
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchPosts: () => dispatch(fetchPosts()),
+        composePost: (data) => dispatch(composePost(data)),
+        updatePost: (data, post) => dispatch(updatePost(data, post)),
+        deletePost: (postId) => dispatch(deletePost(postId))
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(PostIndex);
